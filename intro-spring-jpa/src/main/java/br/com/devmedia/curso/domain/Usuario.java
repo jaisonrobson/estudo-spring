adb1c7375d053e5655ea3d1d787c30c1a6f440d7
jaisonrobson <xd_jrg@hotmail.com>
2021-08-19 17:07:47 -0300

    @intro-spring-jpa -> adicionado dependencias de persistencia ao pom & adicionado anotacoes jpa de persistencia a classe de modelo Usuario

--- intro-spring-jpa/src/main/java/br/com/devmedia/curso/domain/Usuario.java
+++ intro-spring-jpa/src/main/java/br/com/devmedia/curso/domain/Usuario.java
@@ -1,7 +1,10 @@
 package br.com.devmedia.curso.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,22 +12,30 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-public class Usuario {
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(nullable = false, length = 50)
 	@NotBlank
 	@Size(min = 3, max = 50)
 	private String nome;
-	
+
+	@Column(nullable = false, length = 50)
 	@NotBlank
 	@Size(min = 3, max = 50)	
 	private String sobrenome;
-	
+
+	@Column(name = "data_nascimento", nullable = false)
 	@NotNull
 	@DateTimeFormat(iso = ISO.DATE)
 	private LocalDate dtNascimento;
-	
+
+	@Column(name = "tipo_sexo", nullable = false)
+	@Enumerated(EnumType.STRING)
 	private TipoSexo sexo;
 	
 	public Usuario() {
@@ -93,6 +104,19 @@ public class Usuario {
 
 	public void setSexo(TipoSexo sexo) {
 		this.sexo = sexo;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Usuario usuario = (Usuario) o;
+		return id.equals(usuario.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 
 	@Override
