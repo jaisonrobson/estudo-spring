a57db5029b1514f1a6b74b5c138e98a4e7a8d6ca
jaisonrobson <xd_jrg@hotmail.com>
2021-08-21 13:36:24 -0300

    @intro-spring-jpa -> removido implementacao antiga de metodos da classe DAO e implementado metodo getTodos com persistencia a banco de dados

--- intro-spring-jpa/src/main/java/br/com/devmedia/curso/dao/UsuarioDaoImpl.java
+++ intro-spring-jpa/src/main/java/br/com/devmedia/curso/dao/UsuarioDaoImpl.java
@@ -9,61 +9,43 @@ import org.springframework.stereotype.Repository;
 
 import br.com.devmedia.curso.domain.TipoSexo;
 import br.com.devmedia.curso.domain.Usuario;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 @Repository
 public class UsuarioDaoImpl implements UsuarioDao {
 
-	private static List<Usuario> us;
-	
-	public UsuarioDaoImpl() {
-		createUserList();
-	}
-	
-    private List<Usuario> createUserList() {
-        if (us == null) {
-            us = new LinkedList<>();
-            us.add(new Usuario(System.currentTimeMillis()+1L, "Ana", "da Silva", LocalDate.of(1992, 5, 10), TipoSexo.FEMININO));
-            us.add(new Usuario(System.currentTimeMillis()+2L, "Luiz", "dos Santos", LocalDate.of(1990, 8, 11), TipoSexo.MASCULINO));
-            us.add(new Usuario(System.currentTimeMillis()+3L, "Mariana", "Mello", LocalDate.of(1988, 9, 17), TipoSexo.FEMININO));
-            us.add(new Usuario(System.currentTimeMillis()+4L, "Caren", "Pereira"));
-            us.add(new Usuario(System.currentTimeMillis()+5L, "Sonia", "Fagundes"));
-            us.add(new Usuario(System.currentTimeMillis()+6L, "Norberto", "de Souza"));  
-        }
-        return us;
-    }
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Override
 	public void salvar(Usuario usuario) {
-		usuario.setId(System.currentTimeMillis());
-		us.add(usuario);
+
 	}
 
 	@Override
 	public void editar(Usuario usuario) {
-		us.stream()
-			.filter((u) -> u.getId().equals(usuario.getId()))
-			.forEach((u) -> {
-				u.setNome(usuario.getNome());
-				u.setSobrenome(usuario.getSobrenome());
-				u.setDtNascimento(usuario.getDtNascimento());
-				u.setSexo(usuario.getSexo());
-			});		
+
 	}
 
 	@Override
 	public void excluir(Long id) {
-		us.removeIf((u) -> u.getId().equals(id));		
+
 	}
 
 	@Override
 	public Usuario getId(Long id) {
-		return us.stream()
-				.filter((u) -> u.getId().equals(id))
-				.collect(Collectors.toList()).get(0);
+		return null;
 	}
 
+	@Transactional(readOnly = true)
 	@Override
 	public List<Usuario> getTodos() {
-		return us;
-	}	
+		String jpql = "from Usuario u";
+		TypedQuery<Usuario> query = entityManager.createQuery(jpql, Usuario.class);
+		return query.getResultList();
+	}
 }
